4c452332ea4dbb9fc56463fb24264612b54c940c
Dominik Fuchß <develop@fuchss.org>
2017-11-14 00:01:28 +0100

    Updated versions. Minor fix in base class.

--- src/main/java/org/fuchss/swt/SWTShell.java
+++ src/main/java/org/fuchss/swt/SWTShell.java
@@ -11,65 +11,66 @@ import org.eclipse.swt.widgets.Shell;
  *
  */
 public abstract class SWTShell extends Shell {
-    /**
-     * Create a {@link SWTShell}.
-     *
-     * @param display
-     *            the display
-     * @param style
-     *            the style
-     * @see Shell#Shell(Display, int)
-     */
-    protected SWTShell(Display display, int style) {
-        super(display, style);
-        this.createContents();
-    }
+	/**
+	 * Create a {@link SWTShell}.
+	 *
+	 * @param display
+	 *            the display
+	 * @param style
+	 *            the style
+	 * @see Shell#Shell(Display, int)
+	 */
+	protected SWTShell(Display display, int style) {
+		super(display, style);
+		this.createContents();
+	}
 
-    /**
-     * Create a {@link SWTShell}.
-     *
-     * @param shell
-     *            the shell
-     * @param style
-     *            the style
-     * @see Shell#Shell(Shell, int)
-     */
-    protected SWTShell(Shell shell, int style) {
-        super(shell, style);
-    }
+	/**
+	 * Create a {@link SWTShell}.
+	 *
+	 * @param shell
+	 *            the shell
+	 * @param style
+	 *            the style
+	 * @see Shell#Shell(Shell, int)
+	 */
+	protected SWTShell(Shell shell, int style) {
+		super(shell, style);
+		this.createContents();
+	}
 
-    /**
-     * Will invoked once to create contents of the shell.
-     */
-    protected abstract void createContents();
+	/**
+	 * Will invoked once to create contents of the shell.
+	 */
+	protected abstract void createContents();
 
-    /**
-     * Open Shell and update it periodically.
-     */
-    public void startEventLoop() {
-        this.open();
-        this.layout();
-        Display display = this.getDisplay();
-        while (!this.isDisposed()) {
-            if (!display.readAndDispatch()) {
-                Thread.yield();
-            }
-        }
-    }
+	/**
+	 * Open Shell and update it periodically.
+	 */
+	public void startEventLoop() {
+		this.open();
+		this.layout();
+		Display display = this.getDisplay();
+		while (!this.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				Thread.yield();
+			}
+		}
+	}
 
-    /**
-     * Set position to center.
-     */
-    public final void centerMe() {
-        Monitor mon = Display.getDefault().getMonitors()[0];
-        int newLeftPos = (mon.getBounds().width - this.getSize().x) / 2;
-        int newTopPos = (mon.getBounds().height - this.getSize().y) / 2;
-        this.setLocation(newLeftPos, newTopPos);
+	/**
+	 * Set position to center.
+	 */
+	public final void centerMe() {
+		Monitor mon = Display.getDefault().getMonitors()[0];
+		int newLeftPos = (mon.getBounds().width - this.getSize().x) / 2;
+		int newTopPos = (mon.getBounds().height - this.getSize().y) / 2;
+		this.setLocation(newLeftPos, newTopPos);
 
-    }
+	}
 
-    @Override
-    protected final void checkSubclass() {
-        // Disable the check that prevents subclassing of SWT components
-    }
+	@Override
+	protected final void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
 }
